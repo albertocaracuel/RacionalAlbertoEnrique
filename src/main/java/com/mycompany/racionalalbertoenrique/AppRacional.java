0f362cee2ba7539ff0c21c115cf52bbed91143ac
alberto <albertocaracuel16@gmail.com>
2020-12-11 16:28:58 +0100

    Creacion racional k y z

---
+++ src/main/java/com/mycompany/racionalalbertoenrique/AppRacional.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.racionalalbertoenrique;
+
+/**
+ *
+ * @author alber
+ */
+public class AppRacional {
+    public static void main(String[] args) {
+        
+        Racional z = new Racional(4, 5);
+        Racional k = new Racional(3, 4);
+    }
+}
