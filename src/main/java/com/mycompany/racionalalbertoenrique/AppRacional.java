6e98d02da0adae996fc60298d89882d58d6b8b94
alberto <albertocaracuel16@gmail.com>
2020-12-11 16:58:41 +0100

    Division de z y k guardada en un nuevo objeto

--- src/main/java/com/mycompany/racionalalbertoenrique/AppRacional.java
+++ src/main/java/com/mycompany/racionalalbertoenrique/AppRacional.java
@@ -12,30 +12,34 @@ import java.text.DecimalFormat;
  * @author alber
  */
 public class AppRacional {
+
     public static void main(String[] args) {
         DecimalFormat formateador = new DecimalFormat("####.##");
-        
+
         Racional z = new Racional(4, 5);
         Racional k = new Racional(3, 4);
-        
-        Racional r = new Racional();        
+
+        Racional r = new Racional();
         r.aleatorio();
-        
+
         formateador.format(z.getDenominador());
         formateador.format(z.getNumerador());
         z.ImprimirPorConsola();
-        
+
         formateador.format(k.getDenominador());
         formateador.format(k.getNumerador());
         k.ImprimirPorConsola();
-        
+
         formateador.format(r.getDenominador());
         formateador.format(r.getNumerador());
         r.ImprimirPorConsola();
-        
+
         r.igualdad(z, k);
-        
+
         z.suma(k);
-        
+
+        Racional q = new Racional();
+        q.division(z, k);
+
     }
 }
